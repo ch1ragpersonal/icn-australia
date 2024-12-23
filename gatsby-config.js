a5d8ab0bb5e8b4539a9e88ca0f7d2e8ad1a63d77
OrcaStructure <104505623+OrcaStructure@users.noreply.github.com>
2024-12-23 13:54:40 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ICN AUSTRALIA`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms"]
+};
