92be55c6d2b1b5177b3bb98da9fbd042414bd7c1
Chirag Sami <118824187+ch1ragpersonal@users.noreply.github.com>
2025-01-07 15:27:47 +1100

    Added SEO component and updated pages to include seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,8 @@ module.exports = {
   jsxRuntime: 'automatic',
   siteMetadata: {
     title: `ICN AUSTRALIA`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
+    description: `Your site description`,
   },
   plugins: [
     'gatsby-plugin-theme-ui',
@@ -23,6 +24,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-react-helmet`,
     // {
     //   resolve: "gatsby-source-filesystem",
     //   options: {
