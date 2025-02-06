84fbf5ffd93a4332bc4aeff2bbf89654448b17f4
Chirag Sami <118824187+ch1ragpersonal@users.noreply.github.com>
2025-02-06 23:28:48 +1100

    Added button functionality on slider images; resized slider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource theme-ui */
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
-import { Box } from "theme-ui";
+import { graphql, useStaticQuery, Link } from "gatsby";
+import { Box, Button } from "theme-ui";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
@@ -9,86 +9,140 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const ImageSlider = () => {
-  // Fetch images from Contentful using GraphQL
   const data = useStaticQuery(graphql`
     query {
-  allContentfulSliderImage {
-    nodes {
-      id
-      caption
-      image {
-        file {
-          url
+      allContentfulSliderImage {
+        nodes {
+          id
+          caption
+          buttonText
+          buttonLink
+          image {
+            file {
+              url
+            }
+          }
         }
       }
     }
-  }
-}
   `);
 
-  // Transform data into slide format
   const slides = data.allContentfulSliderImage.nodes.map((slide) => ({
     id: slide.id,
     image: slide.image?.file.url
       ? `https:${slide.image.file.url}`
       : "/images/default-placeholder.webp",
     caption: slide.caption || "No Caption",
+    buttonText: slide.buttonText,
+    buttonLink: slide.buttonLink,
   }));
 
-  
-
-  slides.forEach(element => {
-    console.log(element.image)
-  });
   return (
-    <>
-      <Box sx={{ height: "50px" }} />
-      <Box
-        sx={{
-          mt: "40px",
-          width: "100%",
-          maxWidth: "1200px",
-          margin: "0 auto",
+    <Box
+      sx={{
+        width: "100vw",
+        position: "relative",
+        left: "50%",
+        right: "50%",
+        marginLeft: "-50vw",
+        marginRight: "-50vw",
+        marginTop: 0,
+      }}
+    >
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop={true}
+        spaceBetween={0}
+        slidesPerView={1}
+        style={{ 
+          height: "calc(100vh - 80px)",
         }}
       >
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 3000 }}
-          loop={true}
-          spaceBetween={30}
-          slidesPerView={1}
-          style={{ height: "400px" }}
-        >
-          {slides.map((slide) => (
-            <SwiperSlide key={slide.id}>
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <Box
+              sx={{
+                height: "100%",
+                width: "100%",
+                position: "relative",
+                overflow: "hidden",
+              }}
+            >
               <Box
                 sx={{
-                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
                   backgroundImage: `url(${slide.image})`,
                   backgroundSize: "cover",
                   backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                }}
+              />
+              <Box
+                sx={{
+                  position: "relative",
+                  height: "100%",
                   display: "flex",
+                  flexDirection: "column",
                   alignItems: "center",
                   justifyContent: "center",
+                  zIndex: 2,
+                  gap: "20px",
                 }}
               >
                 <h2
                   sx={{
                     color: "white",
                     textShadow: "0px 0px 5px rgba(0,0,0,0.7)",
-                    fontSize: "24px",
+                    fontSize: "32px",
+                    padding: "20px",
+                    backgroundColor: "rgba(0,0,0,0.5)",
+                    borderRadius: "8px",
+                    textAlign: "center",
                   }}
                 >
                   {slide.caption}
                 </h2>
+                {slide.buttonText && slide.buttonLink && (
+                  <Link 
+                    to={slide.buttonLink}
+                    sx={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    <Button
+                      sx={{
+                        backgroundColor: "primary",
+                        color: "white",
+                        padding: "15px 30px",
+                        fontSize: "18px",
+                        fontWeight: "bold",
+                        border: "none",
+                        borderRadius: "4px",
+                        cursor: "pointer",
+                        transition: "transform 0.2s ease-in-out",
+                        "&:hover": {
+                          transform: "scale(1.05)",
+                          backgroundColor: "secondary",
+                        },
+                      }}
+                    >
+                      {slide.buttonText}
+                    </Button>
+                  </Link>
+                )}
               </Box>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </Box>
-    </>
+            </Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
   );
 };
 
