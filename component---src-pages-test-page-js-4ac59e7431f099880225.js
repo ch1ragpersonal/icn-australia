fe256d3274cf95c969b89a99aea7ac5caada7b66
OrcaStructure <104505623+OrcaStructure@users.noreply.github.com>
2025-02-09 22:00:19 +1100

    Updates

---
+++ component---src-pages-test-page-js-4ac59e7431f099880225.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkicn_australia=self.webpackChunkicn_australia||[]).push([[505],{6233:function(r,a,e){e.r(a),e.d(a,{default:function(){return c}});var i=e(6835),n=(e(2671),e(7528)),t=(e(2022),e(853));function c(){return(0,t.FD)(t.FK,{children:[(0,t.Y)(n.A,{title:"Test Page",description:"This is a test page for ICN Australia"}),(0,t.Y)(i.az,{sx:{backgroundColor:"primary",padding:4},children:(0,t.Y)(i.DZ,{sx:{color:"background"},children:"Welcome to My Site"})})]})}}}]);
+//# sourceMappingURL=component---src-pages-test-page-js-4ac59e7431f099880225.js.map
