2aff8dcb43ced077726800bea165b3f777d5c3b8
Chirag Sami <118824187+ch1ragpersonal@users.noreply.github.com>
2025-04-18 16:18:51 +1000

    Add president cards

--- src/components/Presidents.js
+++ src/components/Presidents.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Image, Link } from "theme-ui";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import "./presidentCard.css";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
-const PresidentCard = () => {
-    return (
-        <h1>Hi</h1>
-
-    );
+const PresidentCard = ({ president }) => {
+  const { name, title, photo, bio, contact } = president;
+  console.log("📸 president.photo is:", president.photo);
+  
+  return (
+    <div className="president-card">
+      {photo && (
+        <Image src={photo} alt={name} className="president-photo" sx={{
+          display: 'block',
+          mx: 'auto',           // shorthand for margin-left/right: auto
+          width: '100%',
+          height: '200px',
+          objectFit: 'cover',
+          objectPosition: 'center',
+        }}/>
+      )}
+      <div className="card-content">
+        <h2 className="president-name">{name}</h2>
+        <h3 className="president-title">{title}</h3>
+        <div className="president-description">
+          {bio && documentToReactComponents(JSON.parse(bio.raw))}
+        </div>
+        <div className="contact-links">
+          <Link
+              href={'mailto:' + contact}
+              sx={{ color: "primary" }}
+                >
+              <FaEnvelope />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PresidentCard;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,71 @@
-import React from "react";
-import Seo from "../components/seo";
-import ImageSlider from "../components/ImageSlider";
-import EventShowcase from "../components/EventShowcase";
-import RecentLivestreams from "../components/RecentLivestreams";
-import RecentResults from "../components/RecentResults";
-import { Box, Heading, Text, Grid } from 'theme-ui'
-import ContactUs from "../components/ContactUs";
+// src/pages/contact.js
+import React from 'react';
+import Seo from '../components/seo';
+import ContactUs from '../components/ContactUs';
+import { Box, Heading } from 'theme-ui';
+import { graphql, useStaticQuery } from 'gatsby';
+import PresidentCard from '../components/Presidents';
+import "../styles/contact.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const contactPage = () => {
+const ContactPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulPresident {
+        nodes {
+          id
+          photo {
+            file { url }
+          }
+          bio { raw }
+          contact
+          name
+          title
+        }
+      }
+    }
+  `);
 
-    return(
-        <>
-        <Seo 
-            title="Contact Us"
-            description="Contact ICN Australia"
-          />
-          <Box sx={{ padding: 4 }}>
-            <ContactUs/>
-        </Box>
-        </>
-    );
+  const presidents = data.allContentfulPresident.nodes.map(p => ({
+    ...p,
+    photo: p.photo?.file?.url,
+  }));
 
+  return (
+    <>
+      <Seo title="Contact Us" description="Contact ICN Australia" />
+      <Box sx={{ px: 4, py: 3 }}>
+        <ContactUs />
+        <Heading as="h1" sx={{ mb: 3,
+        px: 4, }}>
+          Meet our Presidents
+        </Heading>
+        <Swiper
+          modules={[Navigation, Pagination, A11y]}
+          spaceBetween={20}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          breakpoints={{
+            320:  { slidesPerView: 1 },
+            640:  { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          style={{ padding: '1rem 0'}}
+        >
+          {presidents.map(pres => (
+            <SwiperSlide key={pres.id}>
+              <PresidentCard president={pres} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+    </>
+  );
+};
 
-}
-
-export default contactPage
+export default ContactPage;
